a4f34157de706b14caedf3aea6987afc816242c1
Emerosn <hentailivre@gmail.com>
2023-04-22 17:19:07 -0300

    Add route for files list

--- main.py
+++ main.py
@@ -2,18 +2,38 @@ import gzip
 import subprocess
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
+
+# Request return init
 @app.get("/")
 def root():
-    print("Resquest Sucessfuk")
-    return 200
+    return{"request: True"} 
 
 
-@app.post("/compress")
+# List files on directory send head param
+@app.get("/listfiles/")
+def get_list(path: str):
+    try:
+        output = subprocess.check_output(["ls","-a",path]).decode("utf-8") 
+        print(output.splitlines())
+    except Exception as e:
+        print(e)
+    return {"files":output.splitlines()}
+
+
+# Compress  on format gz file 
+@app.post("/compress/")
 def compress_file(path: str):
-    with open(path,'rb') as list_files:
-        with gzip.open(path+'.gz','wb') as list_comprimido:
-            list_comprimido.write(list_files.read())
+    try:
+        with open(path,'rb') as list_files:
+            with gzip.open(path+'.gz','wb') as list_comprimido:
+                list_comprimido.write(list_files.read())
+    except Exception  as e:
+        print(e)
     return path + ".gz"
+
+
+# Send backup for one local directory or remote directory
 
