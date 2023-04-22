f9f04a657db70035a0a49e98d8b63c89ae71e7f4
Emerosn <hentailivre@gmail.com>
2023-04-22 04:10:29 -0300

    route for compression from method POST

---
+++ main.py
@@ -0,0 +1,19 @@
+import gzip
+import subprocess
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    print("Resquest Sucessfuk")
+    return 0
+
+
+@app.post("/compress")
+def compress_file(path: str):
+    with open(path,'rb') as list_files:
+        with gzip.open(path+'.gz','wb') as list_comprimido:
+            list_comprimido.write(list_files.read())
+    return path + ".gz"
+
