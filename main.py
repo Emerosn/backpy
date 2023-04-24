dfddc8b7d9e03dc2bf4f1c96d29ac3b97e44d52f
Emerosn <hentailivre@gmail.com>
2023-04-24 00:28:15 -0300

    add new functionality to send files from backups

--- main.py
+++ main.py
@@ -16,36 +16,44 @@ def root():
 @app.get("/listfiles/")
 def get_list(path: str):
     try:
-        output = subprocess.run(
-                [
-                    "ls",
-                    "-a",
-                    path
-                ],
-                    check=True,
-                    capture_output=True
-                ).decode("utf-8") 
-
-        return {output.splitlines()}
-
+        output = subprocess.check_output(["ls","-a",path]).decode("utf-8") 
+        return output.splitlines()
     except Exception as e:
-    
         return e
 
 
 # Compress  on format gz file 
 @app.post("/compress/")
-def compress_file(path: str):
+def compress_file(t,path: str):
     try:
-    
         with open(path,'rb') as list_files:
             with gzip.open(path+'.gz','wb') as list_comprimido:
                 list_comprimido.write(list_files.read())
         output=path+".gz"
         return output
-
     except Exception  as e:
-        
         return e
+
+
+@app.post("/send/")
+def send_file(path_in: str,path_out: str):
+    try:
+        output = subprocess.check_output(["rsync","-avuz",pathin,pathout]).decode("utf-8")
+        return output
+    except Exception as e:
+        return e
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
